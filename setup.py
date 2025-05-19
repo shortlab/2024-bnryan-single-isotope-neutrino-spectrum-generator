64515d761cf69cdb0e135bce6eee3da9ef3809e7
Brianna Ryan <55677131+supernova7000@users.noreply.github.com>
2025-05-19 15:17:16 +0200

    User Friendly
    Making some of my code more "user friendly" as well as adding the neutrino spectra csv files.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='sins',
+    version='0.1',    
+    description='A python pacakged designed to create neutrino spectra for ' + \
+                'the radioactive decay of any isotope.',
+    url='https://github.com/shortlab/2024-bnryan-single-isotope-neutrino-spectrum-generator',
+    author='Brianna Noelani Ryan',
+    author_email='bnryan@mit.edu',
+    #license='BSD 2-clause',
+    packages=['sins'],
+    install_requires=['matplotlib',
+                      'numpy', 
+                      'scipy',  
+                      'scikit-learn',                    
+                      ]
+
+    #classifiers=[
+    #    'Development Status :: 1 - Planning',
+    #    'Intended Audience :: Science/Research',
+    #    'License :: OSI Approved :: BSD License',  
+    #    'Operating System :: POSIX :: Linux',        
+    #    'Programming Language :: Python :: 2',
+    #    'Programming Language :: Python :: 2.7',
+    #    'Programming Language :: Python :: 3',
+    #    'Programming Language :: Python :: 3.4',
+    #    'Programming Language :: Python :: 3.5',
+    #],
+)
---
+++ sins/__init__.py
@@ -0,0 +1,10 @@
+"""
+SINS (Single Isotope Neutrino Spectrum generator)
+
+A python pacakged designed to create neutrino spectra for 
+the radioactive decay of any isotope.
+"""
+
+__version__ = "0.1"
+__author__ = 'Brianna Noelani Ryan'
+__credits__ = 'MIT Laboratory of Nuclear Science'
